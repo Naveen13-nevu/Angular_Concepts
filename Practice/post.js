5c1981300872a767de660c3cf5a74c7747e5dff9
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-07-15 18:24:57 +0530

    update

---
+++ Practice/post.js
@@ -0,0 +1,14 @@
+const user = {
+    name: "Naveen",
+    email: "naveen@gmail.com"
+};
+
+fetch("https://jsonplaceholder.typicode.com/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(user)
+})
+.then(response => response.json())
+.then(data => console.log(data));
