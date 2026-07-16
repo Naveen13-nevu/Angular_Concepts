f23a947ebd7b6b7ad041e2aa8b391b5f2c20dc87
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-07-16 10:33:39 +0530

    update

---
+++ JAVA/uniqueElement.java
@@ -0,0 +1,24 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class uniqueElement{
+    public static void main(String[] args) {
+        
+        int arr[] = {1,2,2,3,3,4,4,5};
+
+        Map<Integer, Integer> unique = new HashMap<>();
+
+        for(int num : arr){
+            unique.put(num, unique.getOrDefault(num, 0) + 1);
+        }
+        
+         for(int key : unique.keySet()){
+            if(unique.get(key) == 1){
+                System.out.print(key);
+            }
+         }
+
+    }
+}
