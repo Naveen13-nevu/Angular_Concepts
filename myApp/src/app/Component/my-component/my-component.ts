07c9d0ffe2cf94a9e88079afaec3075e7289690e
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-07-16 18:42:24 +0530

    update

--- myApp/src/app/Component/my-component/my-component.ts
+++ myApp/src/app/Component/my-component/my-component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-my-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './my-component.html',
   styleUrl: './my-component.css',
 })
 export class MyComponent {
 
-    
+    appName = " Interview Happy";
+
+    user = {
+      name:"Naveen",
+      age:12
+    }
+    imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjyHm5fa2g04XnYGbVF6mPQijZIQH6GYGIflu-8uQX_HFtZqfB_mGcvZo&s=10"
+    showMessage(){
+      alert("Hello World")
+    }
+    userName: string ='';
+
+
+    count: number =0;
+    increment(): void{
+      this.count++;
+    }
+
+    decrement(): void{
+      this.count--;
+    }
+
+    isLoggedin = true;
 }
--- myapp/src/app/app.ts
+++ myapp/src/app/app.ts
@@ -1,13 +1,17 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+
 import { MyComponent } from './Component/my-component/my-component';
+import { User } from './service/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MyComponent],
+  imports: [MyComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('myApp');
+
+  private con = inject(User);
+  users = this.con.getUsers();
 }
