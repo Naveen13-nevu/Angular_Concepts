d08fc8d03cb4a8a2cd8f573028bbff66cdfe5d9a
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-07-30 18:34:29 +0530

    update

---
+++ myApp/src/app/view-user/view-user.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-user',
+  imports: [],
+  templateUrl: './view-user.html',
+  styleUrl: './view-user.css',
+})
+export class ViewUser {}
